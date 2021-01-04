bdb7775c36d9e9a481e15c781dceda623b4defdf
Mikayla Castro <mikaylacastro97@gmail.com>
2021-01-04 15:02:30 -0500

    kata

--- index.js
+++ index.js
@@ -460,3 +460,16 @@ function search(budget, prices) {
        
       return str;
     }
+
+    // Supermarket Queue
+
+    function queueTime(customers, n) {
+      var arr = new Array(n).fill(0);
+    
+      for (var i = 0; i < customers.length; i++) {
+        var idx = arr.indexOf(Math.min(...arr));
+        arr[idx] += customers[i];
+      }
+    
+      return Math.max(...arr);
+    }
